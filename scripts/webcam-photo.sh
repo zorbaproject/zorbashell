dc050d844487f28d708a3c88c95bdeab20997d6a
Luca Tringali <tringalinvent@libero.it>
2017-08-25 17:10:23 +0200

    webcam script added, unicode fixed in msg

--- scripts/webcam-photo.sh
+++ scripts/webcam-photo.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
-    #elif 'webcam' in command:
-    #    tmpfile = '/tmp/'+str(datetime.datetime.now())+str(sender)+'.jpeg'
-    #    tmpfile = tmpfile.replace(" ","_")
-    #    tmpfile = tmpfile.replace(":","_")
-    #    os.system("streamer -s 1280x720 -f jpeg -o "+tmpfile)
-    #    bot.sendPhoto(chat_id, open(tmpfile, "rb"), caption = tmpfile)
-    #    os.remove(tmpfile)
+tmpfile='/tmp/'$(date +%d%m%Y%H%M%S)'.jpeg'
+streamer -s 1280x720 -f jpeg -o $tmpfile  > /dev/null 2>&1
+if [ -f $tmpfile ] ; then
+echo "photo://"$tmpfile
+fi
